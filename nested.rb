4485e4a2f4d3722b95e3fd67d0ac6c2a4eff9da9
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-21 05:52:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     return programmer_hash.unshift(
+     return programmer_hash.shift(
        :yukihiro_matsumoto => {
          :known_for => "Ruby",
          :languages => ["LISP", "C"]
@@ -107,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info 
      
 end
 
